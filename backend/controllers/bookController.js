3fff101ef5616fd465d9c4d1f6390000f25560db
Gopal Tiwari <gopaltiwariorange@gmail.com>
2024-04-18 00:42:21 +0530

    razorpay integrated

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,23 +1,103 @@
-const {EVBooking, CNGBooking} = require('../db/Booking');
-
-const evBooking =async(req, res) => {
-    try {
-        const { userId, pumpId, vehicleNo, timeSlot } = req.body;
-        const evBooking = new EVBooking({ userId, pumpId, vehicleNo, timeSlot });
-        await evBooking.save();
-        res.status(201).send('EV Booking created successfully');
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
+const { EVBooking, CNGBooking, Payment } = require("../db/Booking");
+const { razorpay } = require("../config/razorpayConfig");
+const crypto = require("crypto");
+const evBooking = async (req, res) => {
+  console.log("reached");
+  try {
+    const { userId, pumpId, vehicleNo, timeSlot } = req.body;
+    const evBooking = new EVBooking({ userId, pumpId, vehicleNo, timeSlot });
+    await evBooking.save();
+    res.status(201).send("EV Booking created successfully");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 const cngBooking = async (req, res) => {
-    try {
-        const {userId, pumpId, vehicleNo, timeSlot} = req.body;
-        const cngBooking = new CNGBooking({userId, pumpId, vehicleNo, timeSlot});
-        await cngBooking.save();
-        res.status(201).send('CNG Booking created successfully');
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
-module.exports = {evBooking, cngBooking};
+  try {
+    const { userId, pumpId, vehicleNo, timeSlot } = req.body;
+    const cngBooking = new CNGBooking({ userId, pumpId, vehicleNo, timeSlot });
+    await cngBooking.save();
+    res.status(201).send("CNG Booking created successfully");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const createRazorpayOrder = async (req, res) => {
+  const { userId } = req.body;
+  if (!userId)
+    return res
+      .status(400)
+      .json({ message: "Provide user id.", success: false });
+  const options = {
+    amount: 5 * 100,
+    currency: "INR",
+  };
+
+  const { id, status } = await razorpay.orders.create(options);
+  if (status !== "created")
+    return res.status(400).json({ message: "Order is not created" });
+
+  const orderData = {
+    id,
+    amount: 500,
+    currency: "INR",
+    razorpayKeyId: process.env.RAZORPAY_KEY_ID,
+  };
+  const createdOrder = new Payment({
+    userId,
+    paymentDate: Date.now(),
+    totalAmount: 5,
+    razorpayOrderId: id,
+  });
+  const result = await createdOrder.save();
+  if (!result)
+    return res
+      .status(400)
+      .json({ message: "Order is not saved", success: false });
+  return res.status(200).json({ data: orderData, success: true });
+};
+
+const verifySignature = async (req, res) => {
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+    req.body;
+  const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
+  hmac.update(razorpay_order_id + "|" + razorpay_payment_id);
+  const generatedSignature = hmac.digest("hex");
+  if (generatedSignature === razorpay_signature) {
+    await Payment.findOneAndUpdate(
+      { razorpayOrderId: razorpay_order_id },
+      {
+        paymentStatus: "Fully Paid",
+        razorpaySignature: razorpay_signature,
+        razorpayPaymentId: razorpay_payment_id,
+      },
+      { new: true }
+    );
+    return res.status(200).json({ success: true });
+  }
+  return res.status(400).json({ success: false });
+};
+
+const checkStatus = async (req, res) => {
+  const { orderID } = req.body;
+  if (!orderID)
+    return res
+      .status(400)
+      .json({ message: "Provide order ID.", success: false });
+  const order = await Payment.findOne({ razorpayOrderId: orderID });
+  if (order.paymentStatus === "Fully Paid")
+    return res.status(200).json({ message: "Order is paid", success: true });
+  console.log(order);
+  return res
+    .status(400)
+    .json({ message: "Order Payment is pending.", success: false });
+};
+
+module.exports = {
+  evBooking,
+  cngBooking,
+  createRazorpayOrder,
+  verifySignature,
+  checkStatus,
+};
--- backend/db/Booking.js
+++ backend/db/Booking.js
@@ -1,59 +1,96 @@
 // Booking.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Schema for EV bookings
 const EVBookingSchema = new mongoose.Schema({
-    pumpId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'PetrolPump', // Reference to the PetrolPump schema
-        required: true
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the User schema
-        required: true
-    },
-    vehicleNo: {
-        type: String,
-        required: true
-    },
-    timeSlot: {
-        type: String,
-        required: true
-    },
-    bookedAt: {
-        type: Date,
-        default: Date.now()
-    }
+  pumpId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PetrolPump", // Reference to the PetrolPump schema
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to the User schema
+    required: true,
+  },
+  vehicleNo: {
+    type: String,
+    required: true,
+  },
+  timeSlot: {
+    type: String,
+    required: true,
+  },
+  bookedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 // Schema for CNG bookings
 const CNGBookingSchema = new mongoose.Schema({
-    petrolPumpId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'PetrolPump', // Reference to the PetrolPump schema
-        required: true
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the User schema
-        required: true
-    },
-    vehicleNo: {
-        type: String,
-        required: true
-    },
-    timeDetails: {
-        type: String,
-        required: true
-    },
-    bookedAt: {
-        type: Date,
-        default: Date.now()
-    }
+  petrolPumpId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PetrolPump", // Reference to the PetrolPump schema
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to the User schema
+    required: true,
+  },
+  vehicleNo: {
+    type: String,
+    required: true,
+  },
+  timeDetails: {
+    type: String,
+    required: true,
+  },
+  bookedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-const EVBooking = mongoose.model('EVBooking', EVBookingSchema);
-const CNGBooking = mongoose.model('CNGBooking', CNGBookingSchema);
+// payment
+const PaymentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  paymentDate: {
+    type: String,
+    trim: true,
+  },
+  totalAmount: {
+    type: Number,
+  },
+  amountPaid: {
+    type: Number,
+  },
+  razorpayOrderId: {
+    type: String,
+    trim: true,
+  },
+  razorpayPaymentId: {
+    type: String,
+    trim: true,
+  },
+  razorpaySignature: {
+    type: String,
+    trim: true,
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["Pending", "Fully Paid", "Partially Paid"],
+    default: "Pending",
+  },
+});
 
-module.exports = { EVBooking, CNGBooking };
+const Payment = mongoose.model("Payment", PaymentSchema);
+const EVBooking = mongoose.model("EVBooking", EVBookingSchema);
+const CNGBooking = mongoose.model("CNGBooking", CNGBookingSchema);
+
+module.exports = { EVBooking, CNGBooking, Payment };
